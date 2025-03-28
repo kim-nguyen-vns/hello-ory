7b4a6eb382ea7355fd504221d0641e8a47ec863b
Kim Nguyen <kim.nguyen@vnsilicon.net>
2025-03-28 11:32:13 +0700

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module hello-ory
+
+go 1.24.0
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// helloHandler responds to HTTP requests with "Hello, World!"
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.Header)
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	// Register handler function for the root URL path
+	http.HandleFunc("/hello", helloHandler)
+
+	// Start the HTTP server on port 8080
+	fmt.Println("Server is running on port 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
